0f2b544cf0557a8b24dc1fe6f67cad4c4fc4c52e
mazyar <mazyar.zinweb@gmail.com>
2026-09-22 18:07:14 +0330

    Update Nerkh gold API endpoint

--- api/gold.js
+++ api/gold.js
@@ -1,18 +1,17 @@
 export default async function handler(req, res) {
   try {
-    const apiCode = process.env.ZzrRpqSkOhfFY3dVWNhTBwo3KzBMNtUzaWm3tSdpgY
-
     const response = await fetch(
       "https://nerkh-api.ir/api/ZzrRpqSkOhfFY3dVWNhTBwo3KzBMNtUzaWm3tSdpgY/gold/"
     );
 
-    const json = await response.json();
+    const data = await response.json();
 
-    return res.status(200).json(json);
+    return res.status(200).json(data);
 
   } catch (error) {
     return res.status(500).json({
-      error: error.message
+      error: "خطای سرور",
+      details: error.message
     });
   }
 }
