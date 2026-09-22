4b4cb5d06e61fa376d7f60caab608e528aacf56c
mazyar <mazyar.zinweb@gmail.com>
2026-09-22 17:42:24 +0330

    Show API error details

--- api/gold.js
+++ api/gold.js
@@ -29,8 +29,9 @@ export default async function handler(req, res) {
     });
 
   } catch (error) {
-    return res.status(500).json({
-      error: "خطای سرور"
-    });
-  }
+  return res.status(500).json({
+    error: "خطای سرور",
+    details: error.message
+  });
+}
 }
